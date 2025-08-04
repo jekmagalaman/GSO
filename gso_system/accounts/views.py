5c2c2fd9ba8df4c75f818c74bb7a836921fccf04
KDNLP <kenndainielpaduga@gmail.com>
2025-08-04 15:41:41 +0800

    Update lang ulit sa Unit Head

--- gso_system/accounts/views.py
+++ gso_system/accounts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
 @login_required
 def role_redirect(request):
     user = request.user
